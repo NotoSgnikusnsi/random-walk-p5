d5b4a6d29a3640bd3031db61eda568ba253e9b1a
notosgnikusnsi <notosgnikusnsi@gmail.com>
2023-12-01 13:55:48 +0900

    feat: 進行方向が上下左右4方向のランダムウォークを実装する

--- app.js
+++ app.js
@@ -1,5 +1,47 @@
+// Waker class
+class Walk {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.x = width / 2;
+    this.y = height / 2;
+  }
+
+  walker() {
+    let choice = floor(random(4));
+    switch (choice) {
+      case 0:
+        this.x++;
+        break;
+      case 1:
+        this.x--;
+        break;
+      case 2:
+        this.y++;
+        break;
+      case 3:
+        this.y--;
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  display() {
+    point(this.x, this.y);
+  }
+}
+
+let w;
+
 function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  w = new Walk();
 }
 
 function draw() {
+  w.walker();
+  w.display();
 }
